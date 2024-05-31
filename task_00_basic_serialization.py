c21418505fcc9e1bb5695d42aa6391ee0f574765
DeanSingular <DMabuela@singular.co.za>
2024-05-31 07:57:42 +0000

    Python - Serialization

--- task_00_basic_serialization.py
+++ task_00_basic_serialization.py
@@ -1,24 +1,26 @@
 import json
 
 def serialize_and_save_to_file(data, filename):
-    """Serializes the data to JSON format and saves it to a file."""
-    try:
-        with open(filename, 'w') as file:
-            json.dump(data, file)
-        print(f"Data has been serialized and saved to '{filename}'")
-    except IOError as e:
-        print(f"An error occurred while saving to file: {e}")
+    """
+    Serialize a Python dictionary to a JSON file.
+
+    Parameters:
+    data (dict): A Python dictionary with data.
+    filename (str): The filename of the output JSON file. If the output file already exists it should be replaced.
+    """
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
 def load_and_deserialize(filename):
-    """Loads data from a JSON file and deserializes it to a Python object."""
-    try:
-        with open(filename, 'r') as file:
-            data = json.load(file)
-        return data
-    except IOError as e:
-        print(f"An error occurred while loading from file: {e}")
-        return None
-    except json.JSONDecodeError as e:
-        print(f"An error occurred while decoding JSON: {e}")
-        return None
+    """
+    Load and deserialize data from a JSON file to a Python dictionary.
 
+    Parameters:
+    filename (str): The filename of the input JSON file.
+
+    Returns:
+    dict: A Python dictionary with the deserialized JSON data from the file.
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
